dae3ff151e45d5572ee2477d5c140de0e8a7e387
Pliqu3011 <matthijs3011@gmail.com>
2014-05-14 17:58:18 +0200

    Tellen van rijen en kolommen toegevoegd.

--- opdracht/opdracht2.pl
+++ opdracht/opdracht2.pl
@@ -1,5 +1,5 @@
-% Stijn Manhaeve	20121442
-% Matthijs Van Os	20121014
+% Stijn Manhaeve    20121442
+% Matthijs Van Os   20121014
 %
 % opdracht Programming Paradigms 2013-2014
 % Prolog assignment:
@@ -17,21 +17,21 @@ fillList(X, N, [X|List]) :- N > 0, M is N-1, fillList(X, M, List).
 
 %% nth0 equivalent for matrices
 m_nth0(X, Y, LList, Elem) :- nth0(X, List, Elem),
-							nth0(Y, LList, List).
+                            nth0(Y, LList, List).
 
 %% surrounds the field with water
-surroundRow(Row, SurRow) :- append(['~'|Row], ['~'], SurRow),!.
+surroundList(Row, Buffer, SurRow) :- append([Buffer|Row], [Buffer], SurRow),!.
 
 surroundRows([],[]).
-surroundRows([Head|Tail], [WaterHead|WaterTail]) :- surroundRow(Head, WaterHead),
-													surroundRows(Tail, WaterTail),!.
+surroundRows([Head|Tail], [WaterHead|WaterTail]) :- surroundList(Head, '~', WaterHead),
+                                                    surroundRows(Tail, WaterTail),!.
 
-surroundCols([Row|Rows], SurRows) :- 	length(Row, Width),
-								fillList('~', Width, WaterRow),
-								append([WaterRow|[Row|Rows]], [WaterRow], SurRows),!.
+surroundCols([Row|Rows], SurRows) :-    length(Row, Width),
+                                        fillList('~', Width, WaterRow),
+                                        append([WaterRow|[Row|Rows]], [WaterRow], SurRows),!.
 
-surroundBuffer(Field, WaterField) :- surroundCols(Field, WaterFieldTemp),
-									surroundRows(WaterFieldTemp, WaterField).
+surroundBuffer(Field, WaterField) :-    surroundCols(Field, WaterFieldTemp),
+                                        surroundRows(WaterFieldTemp, WaterField).
 
 
 %% all elements of ships
@@ -77,15 +77,47 @@ checkRow([[LU,U,RU|TailU],[L,C,R|TailC],[LL,D,RL|TailL]]) :-    neighbourRule(C,
 checkRows([Upper,Center,Lower]) :- checkRow([Upper,Center,Lower]).
 checkRows([Upper,Center,Lower|Tail]) :- checkRow([Upper,Center,Lower]), checkRows([Center,Lower|Tail]).
 
-%% WIP, incomplete
-checkValidField(Field,BufferField) :- 	surroundBuffer(Field, BufferField),
-										checkRows(BufferField).
+%% wip incomplete
+checkValidField(Field, RowCount, ColCount, BufferField) :-  surroundBuffer(Field, BufferField),
+                                                            surroundList(RowCount, 0, BufRC),
+                                                            surroundList(ColCount, 0, BufCC),
+                                                            checkRows(BufferField),
+                                                            printField(BufferField),
+                                                            countRows(BufferField, RC),
+                                                            countCols(BufferField, CC),
+                                                            write('COMPARE R:\n\t'),write(RC),nl,write('\t'),write(BufRC),nl, RC == BufRC, 
+                                                            write('COMPARE C:\n\t'),write(CC),nl,write('\t'),write(BufCC),nl, CC == BufCC,
+                                                            write('SUCCESS\n'),!.
 
 %% counts the amount of ship parts are present in a single row
 countRowShipElems([], 0).
 countRowShipElems(['~'|Tail],N):- !, countRowShipElems(Tail, N).
 countRowShipElems([_|Tail],N) :- countRowShipElems(Tail, M), N is M+1.
 
-countColShipElems([],0,_).
+%% counts the amount of ship parts in all rows of a field
+countRows([Row], [N]) :- countRowShipElems(Row, N).
+countRows([Row|Tail], [N|Counts]) :-    countRows(Tail, Counts),
+                                        countRowShipElems(Row, N),!.
+
+%% counts the amount of ship parts in a single column
+countColShipElems([], _, 0).
+countColShipElems([Row|Rows], N, ShipAmount) :- nth0(N, Row, Cell), 
+                                                countColShipElems(Rows, N, M),
+                                                ( shipPart(Cell) -> ShipAmount is M+1 ; ShipAmount is M ).
+
+countCols(Rows, Counts) :-  length(Rows, Length),
+                            Height is Length-1,
+                            countAllCols(Rows, Height, Counts).
+
+countAllCols(Rows, 0, [N]) :- countColShipElems(Rows, 0, N),!.
+countAllCols(Rows, ColNum, NewCounts) :-    Prev is ColNum - 1,
+                                            countAllCols(Rows, Prev, Counts),
+                                            countColShipElems(Rows, ColNum, N),
+                                            append(Counts, [N], NewCounts).
+
+printList([]).  
+printList([X|List]) :-  write(X), write(' '), printList(List).
+printField([]).
+printField([Head|Rows]) :- printList(Head), nl, printField(Rows).
 
 %battleship(Field,Colnums,Rownums,Shipsizes,Field) :- 
